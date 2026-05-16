9491fad8591c46485df8d21f4f6173d35ab52fdd
Anurag-3112 <ak7546824@gmail.com>
2026-05-16 13:04:09 +0530

    Create contactRoutes.js

---
+++ server/Routes/contactRoutes.js
@@ -0,0 +1,11 @@
+ const express = require("express");
+const router = express.Router();
+
+const {createContact, getContact, updateContact, deleteContact}=  require ("../Controllers/contactController");
+
+router.post("/", createContact );
+router.get("/", getContact );
+router.put("/:id", updateContact);
+router.delete("/:id", deleteContact);
+
+module.exports = router;
