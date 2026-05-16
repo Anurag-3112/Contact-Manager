58a21139d748a1217b322fdb7549a2d535f4bb85
Anurag-3112 <ak7546824@gmail.com>
2026-05-16 13:04:12 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const contactRoutes = require("./routes/contactRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/contacts", contactRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
